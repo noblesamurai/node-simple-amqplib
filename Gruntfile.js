7036ef1e104059c48df28362683b2b5d4e57764a
Tim Allen <tim@noblesamurai.com>
2013-12-11 15:36:10 +1100

    Tests, grunt, fix bug found.

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    env: {
+      test: {
+        AMQP_URL: '
+        AMQP_EXCHANGE: 'samuraicloud',
+        AMQP_CONSUME_QUEUE: 'consume_queue',
+        AMQP_PUBLISH_QUEUE: 'publish_queue',
+        AMQP_PUBLISH_QUEUE_ROUTING_KEY: 'snapshot_to_gs',
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    mochacli: {
+      all: ['test/**/*.js'],
+      options: {
+        reporter: 'mocha-unfunk-reporter',
+        ui: 'tdd'
+      }
+    },
+    watch: {
+      files: ['lib/**/*.js', 'test/**/*.js'],
+      tasks: 'test'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.registerTask('test', ['env:test', 'mochacli']);
+  grunt.registerTask('dev', ['env:dev', 'exec:dev']);
+  grunt.registerTask('prod', ['env:prod', 'exec:dev']);
+};
