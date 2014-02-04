56fe350c9b8c6749929024017e3d968f9e921740
Tim Allen <tim@noblesamurai.com>
2014-02-04 13:54:38 +1100

    Nicer abstraction.

--- test/amqp.js
+++ test/amqp.js
@@ -1,5 +1,8 @@
 var AMQP = require('../amqp'),
-    expect = require('chai').expect;
+    SandboxedModule = require('sandboxed-module'),
+    expect = require('chai').expect,
+    sinon = require('sinon'),
+    $ = require('lodash');
 
 describe('AMQP', function() {
   describe('#connect', function() {
@@ -21,6 +24,76 @@ describe('AMQP', function() {
       });
     });
   });
+  describe('#consume', function() {
+    function createMockedModuleObject(messageToDeliver, additionals) {
+      var channelMock = {
+        consume: function (a, handleMessage, b) {
+          handleMessage({
+            content: {
+              toString: function() {return messageToDeliver;}
+            }
+          });
+        }
+      };
+
+      return {
+        locals: {
+          channel: $.extend(channelMock, additionals)
+        }
+      };
+    }
+
+    it('if done(err) is called with err === null, calls ack().',
+        function(done) {
+      var ackSpy = sinon.spy(function(message) {
+        done();
+      });
+      var mockedAMQP = SandboxedModule.require('../amqp',
+        // message will be {}. Mock out 'ack' method.
+        createMockedModuleObject('{}', {ack: ackSpy}));
+
+      function myMessageHandler(parsedMsg, cb) {
+        cb();
+      }
+
+      mockedAMQP.consume(myMessageHandler);
+    });
+
+    it('if json unparsable, calls nack() with requeue of false.',
+        function(done) {
+      var nackSpy = sinon.spy(function(message, upTo, requeue) {
+        expect(requeue).to.equal(false);
+        done();
+      });
+
+      var mockedAMQP = SandboxedModule.require('../amqp',
+        // message will be invalid json. Mock out 'nack' method.
+        createMockedModuleObject('nonvalidjson', {nack: nackSpy}));
+
+      function myMessageHandler(parsedMsg, cb) {
+        cb();
+      }
+
+      mockedAMQP.consume(myMessageHandler);
+    });
+    it('if json callback called with err, calls nack() with requeue as given.',
+        function(done) {
+      var nackSpy = sinon.spy(function(message, upTo, requeue) {
+        expect(requeue).to.equal('requeue');
+        done();
+      });
+      var mockedAMQP = SandboxedModule.require('../amqp',
+        // message will be {}. Mock out 'nack' method.
+        createMockedModuleObject('{}', {nack: nackSpy}));
+
+
+      function myMessageHandler(parsedMsg, cb) {
+        cb(new Error('got it bad'), 'requeue');
+      }
+
+      mockedAMQP.consume(myMessageHandler);
+    });
+  });
 });
 
-
+// vim: set et sw=2 ts=2 colorcolumn=80:
