e13e33df4ba0d9cfb3e0f0bd64bca350d7a74090
Tim Allen <tim@noblesamurai.com>
2016-01-04 10:21:24 +1100

    Fix broken tests.

--- test/amqp-promises.js
+++ test/amqp-promises.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var AMQP = require('amqp-promises');
+var AMQP = require('../amqp-promises');
 
 describe('AMQP', function() {
   var config = require('./config');
   var configRKArray = require('./configRKArray');
 
   describe('#connect', function() {
-    it('should return a promise', function() {
+    it('should return a promise', function(done) {
       var amqp = AMQP(config);
-      return amqp.connect();
+      return amqp.connect().then(function() {
+        done();
+      }, done);
     });
   });
 });
