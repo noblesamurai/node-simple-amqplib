7cd8e91ff3e751d9bd5cb1c4c070240983564d65
Tim Allen <tim@noblesamurai.com>
2016-01-06 10:44:24 +1100

    Clean up config in tests.

--- test/config.js
+++ test/config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  url: process.env.AMQP_URL || '
-  exchange: 'mytestexchange',
-  queue: {
-    name: 'myconsumequeue',
-    routingKey: 'myRoutingQueue',
-    options: {deadLetterExchange: 'wow'}
+  good: {
+    url: process.env.AMQP_URL || '
+    exchange: 'mytestexchange',
+    queue: {
+      name: 'myconsumequeue',
+      routingKey: 'myRoutingQueue',
+      options: {deadLetterExchange: 'wow'}
+    }
+  },
+  noRoutingKey: {
+    url: process.env.AMQP_URL || '
+    exchange: 'hasone',
+    queue: {
+      name: 'myconsumequeue',
+      options: {}
+    }
+  },
+  routingKeyArray: {
+    url: process.env.AMQP_URL || '
+    exchange: 'mytestexchange',
+    queue: {
+      name: 'myconsumequeue',
+      routingKey: ['myRoutingKey', 'myRoutingKey2'],
+      options: {deadLetterExchange: 'wow'}
+    }
   }
 };
 
--- test/configNoKey.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  url: process.env.AMQP_URL || '
-  exchange: 'hasone',
-  queue: {
-    name: 'myconsumequeue',
-    options: {}
-  }
-};
