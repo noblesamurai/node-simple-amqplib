b4457d4871689abc8e5af5132aa448c4264995e4
Tim Allen <tim@noblesamurai.com>
2018-03-09 15:54:30 +1100

    Refactor to reduce indent level.

--- amqp.js
+++ amqp.js
@@ -15,119 +15,122 @@ module.exports = function (config) {
 
   var prefetch = config.prefetch || 10;
 
-  var ret = {
-    /**
-     * Connects and remembers the channel.
-     */
-    connect: function (cb) {
-      var d = Deferred();
-      amqp.connect(config.url, createChannel);
+  /**
+   * Connects and remembers the channel.
+   */
+  function connect (cb) {
+    var d = Deferred();
+    amqp.connect(config.url, createChannel);
 
-      function createChannel (err, conn) {
-        debug('createChannel()');
-        if (err) {
-          return d.reject(err);
-        }
-        connection = conn;
-
-        conn.createConfirmChannel(assertExchange);
+    function createChannel (err, conn) {
+      debug('createChannel()');
+      if (err) {
+        return d.reject(err);
       }
+      connection = conn;
 
-      function assertExchange (err, ch) {
-        debug('assertExchange()', ch);
-        if (err) {
-          return d.reject(err);
-        }
-        channel = ch;
-
-        channel.prefetch(prefetch);
-        channel.assertExchange(config.exchange, 'topic', {}, assertQueues);
-      }
-
-      function assertQueues (err) {
-        debug('assertQueues()');
-        if (err) {
-          return d.reject(err);
-        }
-        if (config.queue && config.queue.name) {
-          queueSetup.setupForConsume(channel, config, d.resolver(cb));
-        } else {
-          d.resolve();
-        }
-      }
-      return d.nodeify(cb);
-    },
-
-    close: function (cb) {
-      if (connection) {
-        return connection.close(cb);
-      }
-      cb();
-    },
-
-    /**
-     * Publish a message using the specified routing key.
-     * @param {string} routingKey The name of the queue to use.
-     * @param {string} message The message to publish.
-     * @param {Object} options Any options to pass through to the underlying
-     *                         publish.
-     * @param {Function(err)} callback The callback to call when done.
-     */
-    publish: function (routingKey, message, options, cb) {
-      debug('publish()');
-      var d = Deferred();
-      if (typeof message === 'object') {
-        message = stringifysafe(message);
-      }
-      channel.publish(config.exchange, routingKey, Buffer.from(message),
-        options, d.resolver(cb));
-
-      return d.nodeify(cb);
-    },
-
-    /**
-     * handleMessage() is expected to be of the form:
-     * handleMessage(parsedMessage, callback).
-     * If callback is called with a non-null error, then the message will be
-     * nacked. You can call it like:
-     * callback(err, requeue) in order
-     * to instruct rabbit whether to requeue the message
-     * (or discard/dead letter).
-     *
-     * If not given, requeue is assumed to be false.
-     *
-     * cf http://squaremo.github.io/amqp.node/doc/channel_api.html#toc_34
-     */
-    consume: function (handleMessage, options) {
-      debug('consume()');
-      function callback (message) {
-        function done (err, requeue) {
-          if (requeue === undefined) {
-            requeue = false;
-          }
-          if (err) {
-            return channel.nack(message, false, requeue);
-          }
-          channel.ack(message);
-        }
-
-        try {
-          var messagePayload = message.content.toString();
-          var parsedPayload = JSON.parse(messagePayload);
-          handleMessage(parsedPayload, done);
-        } catch (error) {
-          console.log(error);
-          // Do not requeue on exception - it means something unexpected
-          // (and prob. non-transitory) happened.
-          done(error, false);
-        }
-      }
-
-      channel.consume(config.queue.name, callback, options);
+      conn.createConfirmChannel(assertExchange);
     }
-  };
 
-  return ret;
+    function assertExchange (err, ch) {
+      debug('assertExchange()', ch);
+      if (err) {
+        return d.reject(err);
+      }
+      channel = ch;
+
+      channel.prefetch(prefetch);
+      channel.assertExchange(config.exchange, 'topic', {}, assertQueues);
+    }
+
+    function assertQueues (err) {
+      debug('assertQueues()');
+      if (err) {
+        return d.reject(err);
+      }
+      if (config.queue && config.queue.name) {
+        queueSetup.setupForConsume(channel, config, d.resolver(cb));
+      } else {
+        d.resolve();
+      }
+    }
+    return d.nodeify(cb);
+  }
+
+  function close (cb) {
+    if (connection) {
+      return connection.close(cb);
+    }
+    cb();
+  }
+
+  /**
+   * Publish a message using the specified routing key.
+   * @param {string} routingKey The name of the queue to use.
+   * @param {string} message The message to publish.
+   * @param {Object} options Any options to pass through to the underlying
+   *                         publish.
+   * @param {Function(err)} callback The callback to call when done.
+   */
+  function publish (routingKey, message, options, cb) {
+    debug('publish()');
+    var d = Deferred();
+    if (typeof message === 'object') {
+      message = stringifysafe(message);
+    }
+    channel.publish(config.exchange, routingKey, Buffer.from(message),
+      options, d.resolver(cb));
+
+    return d.nodeify(cb);
+  }
+
+  /**
+   * handleMessage() is expected to be of the form:
+   * handleMessage(parsedMessage, callback).
+   * If callback is called with a non-null error, then the message will be
+   * nacked. You can call it like:
+   * callback(err, requeue) in order
+   * to instruct rabbit whether to requeue the message
+   * (or discard/dead letter).
+   *
+   * If not given, requeue is assumed to be false.
+   *
+   * cf http://squaremo.github.io/amqp.node/doc/channel_api.html#toc_34
+   */
+  function consume (handleMessage, options) {
+    debug('consume()');
+    function callback (message) {
+      function done (err, requeue) {
+        if (requeue === undefined) {
+          requeue = false;
+        }
+        if (err) {
+          return channel.nack(message, false, requeue);
+        }
+        channel.ack(message);
+      }
+
+      try {
+        var messagePayload = message.content.toString();
+        var parsedPayload = JSON.parse(messagePayload);
+        handleMessage(parsedPayload, done);
+      } catch (error) {
+        console.log(error);
+        // Do not requeue on exception - it means something unexpected
+        // (and prob. non-transitory) happened.
+        done(error, false);
+      }
+    }
+
+    channel.consume(config.queue.name, callback, options);
+  }
+
+  return {
+    connect,
+    close,
+    publish,
+    consume
+  };
 };
 
 // vim: set et sw=2:
