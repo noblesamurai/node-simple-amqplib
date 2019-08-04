412c13d1ced9a0a37647b29dce08b3e00bc7368e
Tim Allen <tim@noblesamurai.com>
2019-08-05 09:58:36 +1000

    Make it an ES2015 class.

--- test/amqp-promises.js
+++ test/amqp-promises.js
@@ -6,7 +6,7 @@ const config = require('./config').good;
 describe('AMQP', function () {
   describe('#consume', function () {
     it('should return a promise', function (done) {
-      var amqp = AMQP(config);
+      var amqp = new AMQP(config);
       amqp.connect().then(function () {
         return amqp.consume().then(function () {
           done();
